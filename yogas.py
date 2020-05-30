1402ee01060b37e497a086ff015031e47e1c86dc
kadvar <kadvar@yahoo.com>
2020-05-30 07:02:52 -0700

    yogas init

---
+++ yogas.py
@@ -0,0 +1,6 @@
+#Returns list of yogas#
+
+
+houses_dict_signlon = {'House1': ['Libra', 3.6629645418784094], 'House2': ['Scorpio', 1.5408442522945052], 'House3': ['Sagittarius', 2.0251098186394643], 'House4': ['Capricorn', 3.879007240706926], 'House5': ['Aquarius', 5.754867809076472], 'House6': ['Pisces', 6.159021441151367], 'House7': ['Aries', 3.6629645418784094], 'House8': ['Taurus', 1.5408442522945052], 'House9': ['Gemini', 2.0251098186394643], 'House10': ['Cancer', 3.8790072407069403], 'House11': ['Leo', 5.754867809076501], 'House12': ['Virgo', 6.159021441151339]}
+
+planets_dict_lon_only = {'Sun': ['Capricorn', 279.9196373475607], 'Moon': ['Sagittarius', 269.1425613804137], 'Mars': ['Capricorn', 283.6911963266094], 'Mercury': ['Sagittarius', 258.7059129886326], 'Jupiter': ['Sagittarius', 241.09105316185673], 'Venus': ['Aquarius', 306.0894496658849], 'Saturn': ['Sagittarius', 267.6901331020929], 'Uranus': ['Sagittarius', 250.1264855882071], 'Neptune': ['Gemini', 85.22504147608478], 'Pluto': ['Gemini', 75.25527693994611], 'Rahu': ['Sagittarius', 259.17343174945495], 'Ketu': ['Gemini', 79.17343174945495], 'Chiron': ['Sagittarius', 258.8702714981658]}
