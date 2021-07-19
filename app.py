ad1e4cf594c8d1ea187fa5e8c1ca82f07e89aace
Krishnakanth <krishnakanth.pps@gmail.com>
2021-07-19 20:04:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -195,6 +195,113 @@ def showchart():
 	return render_template('display_chart.html', birth_name=birth_name, dob=dob_jinja, city=city, tob=tob, tz=tz, p_and_h_dict=p_and_h_dict, planets_dict=planets_dict, houses_dict=houses_dict, navamsa_dict=navamsa_dict, progressions_dict_pr=progressions_dict_pr, prg_details=prg_details, p_and_h_dict_transits=p_and_h_dict_transits, hora_dict=hora_dict, drekkana_dict=drekkana_dict, dwa_dict=dwa_dict, trimsamsa_dict=trimsamsa_dict, shadbala_dict=shadbala_dict, png_dict=png_dict, unicode_dict=unicode_dict)
 
 
+@app.route('/api/horoscope')
+def horo():
+	
+	#Get name, location from form
+	json_data = request.json
+	dob = json_data["dob"]
+	tob = json_data["tob"]
+	city=json_data["city"]
+	tz = json_data["tz]
+#	birth_name = request.form['birth_name']
+#	country = request.form['country']
+#	city = request.form['city']
+#	state = request.form['state']
+#	user_dob = request.form['user_dob']
+	print(user_dob);
+#	dob = user_dob.replace("-","/")
+
+#	#Also get the day, month, year for jinja display
+#	date_list = user_dob.split("-")
+#	year = date_list[0]
+#	month = date_list[1]
+#	day = date_list[2]
+
+	#format in dd-mm-yyyy format for jinja template display
+#	dob_jinja = day+"-"+month+"-"+year
+
+	#generate tob in hh:mm format
+#	hour = request.form['tob_hour']
+#	minute = request.form['tob_minute']
+#	#second = request.form['tob_second']
+#	tob = hour+":"+minute
+
+	#generate tz in +/-hh:mm format
+#	tz = request.form['timezone']
+
+	#get prg date details
+#	prg_date_now = request.form.get("prg_date_now") 
+	if prg_date_now is None:
+		prg_day = request.form['prg_day']
+		prg_month = request.form['prg_month']
+		prg_year = request.form['prg_year']
+		prg_date = prg_year+'/'+prg_month+'/'+prg_day
+	else:
+		#set prg date to today
+		prg_date = datetime.datetime.utcnow()
+		prg_date = prg_date.strftime('%Y/%m/%d')
+
+
+	#get all the planet and house positions (raw - no formatting)
+	print("Input Data ***********************************************************");
+	print(dob);
+	print(tob);
+	print(city);
+	print(tz);
+	print("Data End *************************************************************");
+	planets_dict, houses_dict, planets_dict_lon_only, houses_dict_signlon, chart = calc_allpos(dob, tob, city, tz)
+	print(planets_dict);
+	#zodiac sign list
+	zs_list = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces']
+	
+	#dicts to store formatted objects in zodiac sign - [planet_positions] format
+#	p_and_h_dict = {}
+#	navamsa_dict = {}
+
+	#Generate dict for jinja2 with planets, houses and their positions
+#	for sign in zs_list:
+#		p_and_h_dict[sign] = getPrintableObjects(sign, planets_dict, houses_dict)
+
+	#Navamsa dict - keys = zodiac sign; value = list of planets in that navamsa
+#	for sign in zs_list:
+#		navamsa_dict[sign] = navamsa_from_long(sign, planets_dict_lon_only)
+
+	#Progressions dict - calculate progressions
+#	prog_pl_dict = {}
+#	prog_houses_dict = {}
+
+#	prog_pl_dict, prog_houses_dict, prg_details = calc_progressions(dob, tob, city, tz, prg_date)
+
+	#generate printable objects for progressions
+#	progressions_dict_pr = {}
+#	for sign in zs_list:	
+#		progressions_dict_pr[sign] = getPrintableObjects(sign, prog_pl_dict, prog_houses_dict)
+
+	#Generate transit chart 
+#	p_and_h_dict_transits = calc_transits()
+	#print(p_and_h_dict_transits)
+
+	#####Generate Sharvargas####
+#	hora_dict = calc_horas(planets_dict_lon_only)
+#	drekkana_dict = calc_drekkana(planets_dict_lon_only)
+#	dwa_dict = calc_dwadasamsa(planets_dict_lon_only)
+#	trimsamsa_dict = calc_trimsamsa(planets_dict_lon_only, houses_dict_signlon)
+#	saptamsa_dict = calc_saptamsa(planets_dict_lon_only)
+#
+	#Store all varga dicts in one place
+#	shadvarga_dict = {}
+#	shadvarga_dict['hora_dict'] = hora_dict
+#	shadvarga_dict['drekkana_dict'] = drekkana_dict
+#	shadvarga_dict['dwa_dict'] = dwa_dict
+#	shadvarga_dict['navamsa_dict'] = navamsa_dict
+#	shadvarga_dict['trimsamsa_dict'] = trimsamsa_dict
+#	shadvarga_dict['saptamsa_dict'] = saptamsa_dict
+#	shadbala_dict = calc_shadbalas(planets_dict, houses_dict, planets_dict_lon_only, houses_dict_signlon, shadvarga_dict, chart)
+#
+#	return render_template('display_chart.html', birth_name=birth_name, dob=dob_jinja, city=city, tob=tob, tz=tz, p_and_h_dict=p_and_h_dict, planets_dict=planets_dict, houses_dict=houses_dict, navamsa_dict=navamsa_dict, progressions_dict_pr=progressions_dict_pr, prg_details=prg_details, p_and_h_dict_transits=p_and_h_dict_transits, hora_dict=hora_dict, drekkana_dict=drekkana_dict, dwa_dict=dwa_dict, trimsamsa_dict=trimsamsa_dict, shadbala_dict=shadbala_dict, png_dict=png_dict, unicode_dict=unicode_dict)
+	return(planets_dict)
+
 #Displays Shadbala
 @app.route('/shadbala')
 def shadbala():
